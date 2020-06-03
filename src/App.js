3aa8dea537f1d4ad0e0c7b23804b3ab6e40185ba
jason-nordheim <jason.nordheim@gmail.com>
2020-06-03 15:57:08 -0600

    refactored state to the app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './TodoList'
 
 function App() {
+  /* 
+    `useState` accepts an initial value for the state 
+    of that component. 
+
+    It returns an array, where the first value is the variable
+    holding the value of the state, and the second value is 
+    a function we can use to change that state. 
+    */ 
+  const [todos, setTodos] = useState(['todo 1', 'todo 2'])
   return (
     <> {/*  this is an empty element, which allows us to return 
             multuple elements as a single element */}
-      <TodoList /> 
+      <TodoList todos={todos} /> 
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Complete</button>
