3682b42a60572644477c17e23519b004195a1d92
jason-nordheim <jason.nordheim@gmail.com>
2020-06-03 16:43:07 -0600

    basic functionaltiy successfully created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React, {useState, useRef } from 'react';
+import React, {useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
+
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   /* 
@@ -22,18 +26,49 @@ function App() {
   */ 
   const todoNameRef = useRef() 
 
+  /* Allows todo to persist between reloads */
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+    
+  }, []) // by passing an empty array this will only be called 1 time
+
+  /* save todos as they are created */
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodo = [...todos]
+    const todo = newTodo.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodo)
+  }
+
   function handleAddTodo(event) {
     const name = todoNameRef.current.value 
     if (name === '') return // do not create an empty todo 
+    console.log(`todo name ${name}`)
+
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name, complete: false }] 
+    })
+
+    todoNameRef.current.value = null // reset 
+  }
+
+  function handlClearTodos(event) {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
   }
 
   return (
     <> {/*  this is an empty element, which allows us to return 
             multuple elements as a single element */}
-      <TodoList todos={todos} /> 
+      <TodoList todos={todos} toggleTodo={toggleTodo} /> 
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Complete</button>
+      <button onClick={handlClearTodos}>Clear Complete</button>
       <div>0 left to do</div>
     </>
   )
