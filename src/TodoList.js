bfd3f3ee83f8ad402e6bcc97aba76db9073e9bf7
jason-nordheim <jason.nordheim@gmail.com>
2020-06-03 15:59:06 -0600

    added map for each todo passed to the todo list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
-    /* 
-        `useState` accepts an initial value for the state 
-        of that component. 
+export default function TodoList({ todos }) {
 
-        It returns an array, where the first value is the variable
-        holding the value of the state, and the second value is 
-        a function we can use to change that state. 
-    */ 
-    const [todos, setTodos] = useState([])
     return (
-        <div>
-            hello world
-        </div>
+       todos.map(todo => {
+           return <Todo todo={todo} /> 
+       })
     )
 }
